0cc8332525d70042675e9aa7b510763c6d42ad4f
hamza iraqi <hz.iraqi@gmail.com>
2019-03-15 12:15:56 +0100

    added activities

---
+++ app/policies/activity_policy.rb
@@ -0,0 +1,15 @@
+ class ActivityPolicy < ApplicationPolicy
+ class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    record.user == user
+  end
+end
