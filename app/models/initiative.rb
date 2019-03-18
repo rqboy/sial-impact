324333f674978a6277356742267e301a4e775239
hamza iraqi <hz.iraqi@gmail.com>
2019-03-18 12:38:19 +0100

    added users

--- app/models/initiative.rb
+++ app/models/initiative.rb
@@ -1,5 +1,6 @@
 class Initiative < ApplicationRecord
   mount_uploader :photo, PhotoUploader
-  belongs_to :user
+  belongs_to :user, class_name: 'User'
+  belongs_to :referent, class_name: 'User'
   has_many :activities
 end
